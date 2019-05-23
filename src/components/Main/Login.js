a6a4d87b0d7322392c690c92d061027592496c5c
Nat <natalie.hoang91@gmail.com>
2019-05-23 13:45:15 +0700

    fix UI

--- src/components/Main/Login.js
+++ src/components/Main/Login.js
@@ -14,7 +14,7 @@ class Login extends Component {
 
   postUserLogin = data => {
     console.log(JSON.stringify(data))
-    fetch("http://localhost:5000/login", {
+    fetch("hhttps://backend-codteur-project.herokuapp.com/login", {
       method: "POST",
 
       headers: {
